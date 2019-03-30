537cc3100255cda7c2a3cc9b1385db29522bb926
Carter <carter@elhnet.net>
2019-03-29 17:45:43 -0700

    allow users to use differnt hastebin-clone sites

--- hastebingen.js
+++ hastebingen.js
@@ -1,11 +1,12 @@
-const snekfetch = require("snekfetch");
+const { promisified: p } = require('phin');
 
-module.exports = (function (input, extension) {
-    return new Promise(function (res, rej) {
-        if (!input) rej("Input argument is required.");
-        snekfetch.post("https://hastebin.com/documents").send(input).then(body => {
-            res("https://hastebin.com/" + body.body.key + ((extension) ? "." + extension : ""));
-        }).catch(e => rej(e));
-    })
-});
-// Thanks to gus for making Snekfetch a thing kthxs and PassTheMayo for recreating this ^
+module.exports = async function(code, lang = '', site = 'https://hastebin.com/documents'){
+    try{
+        const res = await p({url: site, method: 'POST', data: code})
+        const { key } = JSON.parse(res.body);
+        return `${site}/${key}${lang && `.${lang}`}`
+    }catch(err){
+        throw err;
+    }
+}
+// props to Nomsy for the model :p
